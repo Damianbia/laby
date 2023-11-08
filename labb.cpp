fc9ff603971414f38fb2f3e5eeb7f3fde178828e
Damianbia <147631936+Damianbia@users.noreply.github.com>
2023-11-08 17:29:03 +0100

    Add files via upload

---
+++ labb.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    double xp, xk, krok, xt;
+    
+    do {
+        cout << "podaj xp ";
+        cin >> xp;
+        cout << "podaj xk ";
+        cin >> xk;
+    } while (xk < xp);
+    cout << "podaj krok ";
+    cin >> krok;
+
+    cout << endl << "petla for" <<endl;
+    for (double i = xp; i < xk+0.000000001; i = i + krok)
+    {
+        cout << "dla x=" << i << " x2=" <<( i * i) << " x3=" << i * i * i<<endl;
+    }
+
+    xt = xp;
+    cout << endl << "petla while" << endl;
+    while ( xt<xk+0.00000000001) {
+        cout << "dla x=" << xt << " x2=" << (xt * xt) << " x3=" << xt * xt * xt << endl;
+        xt = xt + krok;
+    }
+
+    xt = xp;
+    cout << endl <<"petla do while" << endl;
+    do {
+        cout << "dla x=" << xt << " x2=" << (xt * xt) << " x3=" << xt * xt * xt << endl;
+        xt = xt + krok;
+    } while (xt < xk + 0.00000000001);
+
+
+}
+
---
+++ labb3.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+
+	int N;
+	double liczba, suma=0, suma2=0, srednia=0;
+	cout << "podaj N ";
+	cin >> N;
+	cout << endl;
+
+	for (int i = 1; i <= N; i++) {
+		cout << "podaj liczbe " << i << " ";
+		cin >> liczba;
+		cout << endl;
+		suma = suma + liczba;
+		if (liczba > 0)
+			suma2 = suma2 + liczba;
+		srednia = srednia + liczba;
+	}
+	cout << endl << "suma wynosi: " << suma << endl << "suma liczb dodatnich wynosi: " << suma2 << endl << "srednia wynosi: " << srednia / N;
+}
