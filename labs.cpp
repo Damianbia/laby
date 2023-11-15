e2959415fc535c2f90d5d58f4cf8eb020b4caa74
Damianbia <147631936+Damianbia@users.noreply.github.com>
2023-11-15 17:35:08 +0100

    Add files via upload

---
+++ labs.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tab[5],ele;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Wartosc elementu " << i+1 << " = ";
+		cin >> ele;
+		tab[i] = ele;
+	}
+	cout << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << tab[i] << " ";
+	}
+
+}
